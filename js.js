2ba2dfbda138e06b6ad6342d89fc2290b8c2d31b
Edward <baseball.tampabayrays@gmail.com>
2019-03-26 18:27:55 -0500

    update

---
+++ js.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  $('a.valueButton').on('click', function() {
+  $('h4.cardname').text('Jace Beleren, Price $7.00');
+  $('.cardinfo').show();
+
+  });
+})
+
+
+
+ //$('li.addcollectiontext').on('mouseover', function(){
+//$(this).text('DID you pack this card?')
+
+
+//});
