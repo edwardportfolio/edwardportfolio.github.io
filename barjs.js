2025c837d73abbe7074cf6fa2c395ce08c4b8bd7
Edward <baseball.tampabayrays@gmail.com>
2019-04-16 09:26:52 -0500

    mtg

---
+++ barjs.js
@@ -0,0 +1,67 @@
+/****
+
+  To pull in data from a CSV file, we wrap our javascript inside a plot.d3.csv() function.
+
+****/
+
+Plotly.d3.csv('data.csv', function(err, rows){
+
+
+  /***
+  This function is useful for returning an array of values corresponding to a column in your CSV file. It's not built in to PLotly, so you have to declare it as follows:
+  ***/
+  var unpack = function(rows, key) {
+    return rows.map(function(row) { return row[key]; });
+  }
+
+
+  /*** Now that we know how to pull the data from a CSV, we can create our data objects as we've done before: ***/
+  var majors = {
+    // x and y are arrays of numeric values, so we can create those using unpack().
+    x: unpack(rows, 'Power'),
+    y: unpack(rows, 'Toughness'),
+    type: 'scatter', // the type of plot you're producing. Scatter is used to plot points with x and y values
+    mode: 'markers', // possible modes: markers, markers+text, lines
+    text: unpack(rows, 'Cards'), // If specified, this is the text that pops up on hover. If not specified, the text is the y-value for the point.
+    name: 'Mtg',
+    transforms: [
+      {
+        type: 'filter',
+        target: 'y',
+        operation: '>',
+        value: 20000
+      }
+    ]
+  };
+
+
+
+
+  /*** Now that we've created our data objects using our CSV, we just create the visualization as we've done before: ***/
+
+  // Create the data object as an array of our data series objects:
+  var data = [majors];
+
+  // The layout object provides options for how our visualization will appear:
+  var layout = {
+    title:'Magic The Gathering',
+    showlegend: false,
+    hovermode: true, // if false, disables the hover text for the entire plot
+    xaxis: {
+     title: 'Power'
+    },
+    yaxis: {
+     title: 'Toughness'
+    }
+  }
+
+  var options = {
+   displayModeBar: false, // disable zoom, save and other toolbar options.
+  }
+
+  Plotly.newPlot('viz', data, layout, options);
+
+
+
+
+})
