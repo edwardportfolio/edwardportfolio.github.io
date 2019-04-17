3c2e4e690089b9679b20205a880da21287bb5c6c
Edward <baseball.tampabayrays@gmail.com>
2019-04-16 20:27:37 -0500

    mtg

--- barjs.js
+++ barjs.js
@@ -21,6 +21,7 @@ console.log(unpack(rows, 'Toughness'));
 
 let powerArray = unpack(rows, 'Power').map(Number)
 let toughnessArray = unpack(rows, 'Toughness').map(Number)
+console.log('!');
   /*** Now that we know how to pull the data from a CSV, we can create our data objects as we've done before: ***/
   var mtg = {
     // x and y are arrays of numeric values, so we can create those using unpack().
